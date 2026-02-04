62fea9c907ec362fcce3c57a098f99cc65b26fab
Shruj-12 <srutikagandhi@gmail.com>
2026-02-03 19:37:02 -0500

    Add files via upload

---
+++ mersenne52.py
@@ -0,0 +1,16 @@
+# Largest Known Prime Number (Mersenne 52)
+# More information at https://www.mersenne.org/
+# More information at https://t5k.org/largest.html#references
+
+import sys
+sys.set_int_max_str_digits(41024321)
+
+print("Max string digits: ", sys.get_int_max_str_digits())
+
+print("!START OF MERSENNE 52!")
+mersenne52 = (2 ** 136279841) - 1
+print(mersenne52)
+
+print("!END OF MERSENNE 52!")
+print("Digits printed: ", len(str(mersenne52)))
+
